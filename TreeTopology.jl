c6c6436bbfefb06e002a2781667d037c4e8e41e7
EasternBoy <binh.bkhn91@gmail.com>
2025-02-03 17:13:49 -0500

    Forward

--- TreeTopology.jl
+++ TreeTopology.jl
@@ -12,44 +12,45 @@ include("node.jl")
 nlocal = 2
 
 D = 3
-rootnode = node(0,-1, [1,2,3], zeros(nlocal), 0.)
-node11   = node(1, 0, [1,2],   zeros(nlocal), 1.1)
-node12   = node(1, 0, Int64[], zeros(nlocal), 1.2)
-node13   = node(1, 0, [3,4],   zeros(nlocal), 1.3)
-node21   = node(2, 1, Int64[], zeros(nlocal), 2.1)
-node22   = node(2, 1, Int64[], zeros(nlocal), 2.2)
-node23   = node(2, 3, [1,2,3], zeros(nlocal), 2.3)
-node24   = node(2, 3, Int64[], zeros(nlocal), 2.4)
-node31   = node(3, 3, Int64[], zeros(nlocal), 3.1)
-node32   = node(3, 3, Int64[], zeros(nlocal), 3.2)
-node33   = node(3, 3, Int64[], zeros(nlocal), 3.3)
+rootnode = node(0, 1,-1, [1,2,3], zeros(nlocal), 0.)
+node11   = node(1, 1, 0, [1,2],   zeros(nlocal), 1.1)
+node12   = node(1, 2, 0, Int64[], zeros(nlocal), 1.2)
+node13   = node(1, 3, 0, [3,4],   zeros(nlocal), 1.3)
+node21   = node(2, 1, 1, Int64[], zeros(nlocal), 2.1)
+node22   = node(2, 2, 1, Int64[], zeros(nlocal), 2.2)
+node23   = node(2, 3, 3, [1,2,3], zeros(nlocal), 2.3)
+node24   = node(2, 4, 3, Int64[], zeros(nlocal), 2.4)
+node31   = node(3, 1, 3, Int64[], zeros(nlocal), 3.1)
+node32   = node(3, 2, 3, Int64[], zeros(nlocal), 3.2)
+node33   = node(3, 3, 3, Int64[], zeros(nlocal), 3.3)
 
 
 tree = [[node11, node12, node13], 
         [node21, node22, node23, node24], 
-        [node31, node32, node33]]
+        [node31, node32, node33]];
 
 for d in D:-1:1
     for i in 1:length(tree[d])
-        p = tree[d][i].parent
         if d == 1
             push!(rootnode.couple_state, vect(tree[d][i].couple_state))
             push!(rootnode.dual_state, vect(tree[d][i].dual_state))
         else
+            p = tree[d][i].parent
             push!(tree[d-1][p].couple_state, vect(tree[d][i].couple_state))
             push!(tree[d-1][p].dual_state, vect(tree[d][i].dual_state))
         end
     end
 end
 
-query!(rootnode, tree)
+tTree = (rootnode, tree)
 
-# #Forward
-# for d in 1:D
-#     n = size(tree[d])
-#     for i in 1:n
-#         response!(tree[d][i])
-#     end
-# end
+prox!(rootnode, tTree)
+
+#Forward
+for d in 1:D
+    for i in 1:length(tree[d])
+        prox!(tree[d][i], tTree)
+    end
+end
 
 #Backward
--- node.jl
+++ node.jl
@@ -2,26 +2,41 @@ using LinearAlgebra, Optim, JuMP, Plots
 
 mutable struct node
     level::Int64
+    index::Int64
     parent::Int64
     children::Vector{Int64}
     local_state::Vector{Float64}
     cost::Float64
-    couple_state::Vector{Vector{Float64}}
-    dual_state::Vector{Vector{Float64}}
+    couple_state
+    dual_state
     opti
 
-    function node(level::Int64, parent::Int64, children::Vector{Int64}, local_state::Vector{Float64}, cost::Float64)
-        obj = new(level, parent, children, local_state, cost)
+    function node(level::Int64, index::Int64, parent::Int64, children::Vector{Int64}, local_state::Vector{Float64}, cost::Float64)
+        obj = new(level, index, parent, children, local_state, cost)
         if length(children) == 0
-            obj.couple_state = [local_state]
+            obj.couple_state = local_state
+            obj.dual_state   = local_state
         else
             obj.couple_state = Vector{Float64}[]
-            dual_state = Vector{Float64}[]
+            obj.dual_state   = Vector{Float64}[]
         end
         
         obj.opti = JuMP.Model(Optim.Optimizer)
         return obj
     end
+end
+
+
+function vect(v::Vector{Vector{Float64}})
+    res = Float64[]
+    for i in 1:length(v)
+        res = [res; v[i]]
+    end
+    return res
+end
+
+function vect(v::Vector{Float64})
+    return v
 end
 
 function query!(nd::node, tree::Vector{Vector{node}})
@@ -32,41 +47,71 @@ function response!(parent::node, tree::Vector{Vector{node}})
     return 0
 end
 
-function prox!(nd::node, tree::Vector{node})
+function prox!(nd::node, tTree::Tuple{node, Vector{Vector{node}}})
+
+    root  = tTree[1]
+    tree  = tTree[2]
+
     nd.opti = JuMP.Model(Optim.Optimizer)
     set_optimizer_attribute(nd.opti, "method", BFGS())
     set_silent(nd.opti)
 
-    
-
-
     λ = 1.
+    d = nd.level
+    p = nd.parent
     n = length(nd.children)
     l = [length(nd.couple_state[i]) for i in 1:n]
     c = nd.cost
 
-    if node.level == 0
-        λ = lam
-        @variable(model, x[i = 1:length(nd.children), 1:l[i]])
+    if d == 0 #Root node
+        @variable(nd.opti, x[i = 1:n, 1:l[i]])
 
-        q = [nd.dual_state[i] + tree[1][i].couple_state for i in 1:n]
+        q = [nd.dual_state[i] + vect(tree[1][i].couple_state) for i in 1:n]
 
-        J = sum(dot(x[i] .- c, x[i] .- c) for i in 1:n) + 1/(2λ)*sum(dot(x[i] - q[i], x[i] - q[i]) for i in 1:n)
-    elseif length(node.children) == 0
-        @variable(model, x[i:length(nd.couple_state)])
+        J = sum(dot(x[i,:] .- c, x[i,:] .- c) for i in 1:n) + 1/(2λ)*sum(sum((x[i,j] - q[i][j])^2 for j in 1:l[i]) for i in 1:n)
     else
-        @variable(model, x[i = 1:length(nd.children), 1:l[i]])
+        parent = (d==1) ? root : tree[d-1][p]
+        if n == 0 #Leaf nodes
+            @variable(nd.opti, x[1:length(nd.couple_state)])
+
+            i = indexin(nd.index, parent.children)[1]
+
+            q = parent.couple_state[i] - parent.dual_state[i]
+
+            J = dot(x .- c, x .- c) + 1/(2λ)*dot(x - q, x - q)
+        else #Middle node
+            @variable(nd.opti, x[i = 1:length(nd.children), 1:l[i]])
+
+            i = indexin(nd.index, parent.children)[1]
+
+            q = parent.couple_state[i] - parent.dual_state[i]
+
+            u = nd.dual_state
+
+            k = 1
+            for i in 1:n
+                u[i] = u[i] + q[k:(k+l[i]-1)]
+                k = k+l[i]
+            end
+
+            xch = [vect(tree[d+1][j].couple_state) for j in nd.children]
+
+            res = xch + u
+
+            J = sum(dot(x[i,:] .- c, x[i,:] .- c) for i in 1:n) + 1/(2λ)*sum(sum((x[i,j] - res[i][j])^2 for j in 1:l[i]) for i in 1:n)
+        end
+    end
+    
+    @objective(nd.opti, Min, J)
+    JuMP.optimize!(nd.opti)
+    x = JuMP.value.(x)
+    if n == 0
+        nd.couple_state = x
+    else
+        for i in 1:n
+            nd.couple_state[i] = [x[i,j] for j in 1:l[i]]
+        end
     end
 
-
-
-    @variable(robo.opti, x[1:n])
-end
-
-function vect(v::Vector{Vector{Float64}})
-    res = Float64[]
-    for i in 1:length(v)
-        res = [res; v[i]]
-    end
-    return res
+    return nd.couple_state
 end
